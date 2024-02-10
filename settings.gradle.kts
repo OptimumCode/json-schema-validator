30ac39676698b47fb64a805c6bcbe968b4dbc3e9
Oleg Smirnov <oleg31101996@gmail.com>
2024-02-10 19:10:01 +0400

    Try without enterpise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
-plugins {
-  id("com.gradle.enterprise") version "3.16.2"
-}
-
-gradleEnterprise {
-  buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
-    isUploadInBackground = false
-  }
-}
+// plugins {
+//  id("com.gradle.enterprise") version "3.16.2"
+// }
+//
+// gradleEnterprise {
+//  buildScan {
+//    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+//    termsOfServiceAgree = "yes"
+//    isUploadInBackground = false
+//  }
+// }
 
 rootProject.name = "json-schema-validator"
 
