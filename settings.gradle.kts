d874686a27755544832a97f71a6a8e90daa8f180
Oleg Smirnov <oleg31101996@gmail.com>
2024-02-10 19:17:30 +0400

    Remove enterprise plugin from settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,3 @@
-// plugins {
-//  id("com.gradle.enterprise") version "3.16.2"
-// }
-//
-// gradleEnterprise {
-//  buildScan {
-//    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-//    termsOfServiceAgree = "yes"
-//    isUploadInBackground = false
-//  }
-// }
-
 rootProject.name = "json-schema-validator"
 
 include(":test-suites")
