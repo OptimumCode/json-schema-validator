3bb0280ce9e880d20cfa6c5c2694adf239ab35c3
Oleg Smirnov <oleg31101996@gmail.com>
2024-04-13 20:42:12 +0400

    Add test for specific cases

--- src/commonTest/kotlin/io/github/optimumcode/json/schema/assertions/general/format/JsonSchemaIdnHostnameFormatValidationTest.kt
+++ src/commonTest/kotlin/io/github/optimumcode/json/schema/assertions/general/format/JsonSchemaIdnHostnameFormatValidationTest.kt
@@ -18,6 +18,10 @@ class JsonSchemaIdnHostnameFormatValidationTest : FunSpec() {
           "xn--80aakdqneodaeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaai3g0b8b9p5ivf",
           // normalized and valid idn hostname
           "\u4E3D",
+          // valid with arabic indic digit
+          "\u0628\u0669",
+          // valid with extended arabic indic digit
+          "\u0628\u06F9",
         ),
       invalidTestCases =
         listOf(
@@ -26,7 +30,10 @@ class JsonSchemaIdnHostnameFormatValidationTest : FunSpec() {
           TestCase("оооооооооооооооооооооооооооооооооооченьдлиннаястрока", "too long unicode"),
           // Not normalized \u4E3D. Example from https://unicode.org/Public/UNIDATA/NormalizationTest.txt
           TestCase(CodePoints.toChars(0x2F800).joinToString(separator = ""), "not normalized"),
-          TestCase("\u0669\u06F9", "mixed arabic indic digits and extended arabic indict digits"),
+          TestCase("\u0628\u0669\u06F9", "mixed arabic indic digits and extended arabic indict digits"),
+          TestCase("\u0628\u0061", "fails BiDi rule RTL"),
+          TestCase("\u0061\u0628", "fails BiDi rule LTR"),
+          TestCase("a\u200C", "zero with non joiner rule does not match"),
         ),
     )
   }
