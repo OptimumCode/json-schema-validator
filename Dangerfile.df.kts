1754020ab8820cee257a581feee18cfd70bc180a
Oleg Smirnov <oleg31101996@gmail.com>
2024-06-22 15:53:33 +0400

    Add PR validations to improve maintainability (#143)
    Resolves #26

---
+++ Dangerfile.df.kts
@@ -0,0 +1,17 @@
+@file:Suppress("ktlint:standard:no-wildcard-imports")
+
+import systems.danger.kotlin.*
+
+danger(args) {
+  onGitHub {
+    val prLabels = issue.labels
+    when (prLabels.size) {
+      0 -> fail("PR must have labels")
+      1 ->
+        prLabels.find { it.name.equals("ignore", ignoreCase = true) }?.let {
+          warn("PR must have labels other then '[${it.name}](${it.url})'")
+        }
+      else -> {}
+    }
+  }
+}
