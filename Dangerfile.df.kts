40cca4bb6c0d4f899a661a56e151d9b6a902e730
Oleg Smirnov <oleg31101996@gmail.com>
2024-06-22 15:42:20 +0400

    Add warning for only ignore label. Fail on missing labels

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -4,8 +4,14 @@ import systems.danger.kotlin.*
 
 danger(args) {
   onGitHub {
-    if (issue.labels.isEmpty()) {
-      warn("PR must have labels")
+    val prLabels = issue.labels
+    when (prLabels.size) {
+      0 -> fail("PR must have labels")
+      1 ->
+        prLabels.find { it.name.equals("ignore", ignoreCase = true) }?.let {
+          warn("PR must have labels other than 'ignore'")
+        }
+      else -> {}
     }
   }
 }
