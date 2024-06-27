8bf3e480c5556dbca5d8f4a2557d3682d4f63fe1
Oleg Smirnov <oleg31101996@gmail.com>
2024-06-27 17:35:12 +0400

    Add check for api changes

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,17 +1,30 @@
 @file:Suppress("ktlint:standard:no-wildcard-imports")
 
 import systems.danger.kotlin.*
+import systems.danger.kotlin.models.github.GitHubIssueLabel
 
 danger(args) {
+  if (git.modifiedFiles.any { it.contains("json-schema-validator/api/") }) {
+    markdown(
+      "PR introduces changes to the public API. " +
+        "Please add **\"ABI breaking\"** label if **any line** in API file was changed or deleted. " +
+        "Please add **\"API breaking\"** label if **any public method** in API file was changed or deleted.",
+    )
+  }
   onGitHub {
     val prLabels = issue.labels
-    when (prLabels.size) {
-      0 -> fail("PR must have labels")
-      1 ->
-        prLabels.find { it.name.equals("ignore", ignoreCase = true) }?.let {
-          warn("PR must have labels other then '[${it.name}](${it.url})'")
-        }
-      else -> {}
-    }
+    checkPrLabels(prLabels)
+  }
+}
+
+fun checkPrLabels(prLabels: List<GitHubIssueLabel>) {
+  when (prLabels.size) {
+    0 -> fail("PR must have labels")
+    1 ->
+      prLabels.find { it.name.equals("ignore", ignoreCase = true) }?.let {
+        warn("PR must have labels other then '[${it.name}](${it.url})'")
+      }
+
+    else -> {}
   }
 }
