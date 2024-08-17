7640873db25531cd07717a714b93a95d57b1d471
Oleg Smirnov <oleg31101996@gmail.com>
2024-08-17 11:50:54 +0400

    Cleanup install script for format tools

--- scripts/install-format-tools.sh
+++ scripts/install-format-tools.sh
@@ -7,19 +7,18 @@ if [[ "$KTLINT_VERSION" == "" ]]; then
 	echo "Provide ktlint version in KTLINT_VERSION env variable"
 	exit 1
 fi
-echo "Installing detekt"
+echo "Installing detekt $DETEKT_VERSION"
 jar_destination="${HOME}/.local/lib/detekt"
 mkdir -p ${jar_destination}
 curl -sSL -o ${jar_destination}/detekt-cli.jar "https://github.com/detekt/detekt/releases/download/v${DETEKT_VERSION}/detekt-cli-${DETEKT_VERSION}-all.jar"
 
 entrypoint_script="${HOME}/.local/bin/detekt-cli"
 cat > "$entrypoint_script" << EOM
-#!/bin/bash
 java -jar ${jar_destination}/detekt-cli.jar \$@
 EOM
 chmod +x "$entrypoint_script"
 detekt-cli --version
 
-echo "Installing ktlint"
+echo "Installing ktlint $KTLINT_VERSION"
 curl -sSLO "https://github.com/pinterest/ktlint/releases/download/$KTLINT_VERSION/ktlint" && chmod a+x ktlint && mv ktlint ~/.local/bin/
 ktlint --version
