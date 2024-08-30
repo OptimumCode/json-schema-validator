e5cf619c2f8809ab688bcb0cc6579b76563c8ef3
Oleg Smirnov <oleg31101996@gmail.com>
2024-08-30 12:32:50 +0400

    Remove references to JsonElement from format validator

--- json-schema-validator/src/commonMain/kotlin/io/github/optimumcode/json/schema/FormatValidator.kt
+++ json-schema-validator/src/commonMain/kotlin/io/github/optimumcode/json/schema/FormatValidator.kt
@@ -1,12 +1,11 @@
 package io.github.optimumcode.json.schema
 
 import io.github.optimumcode.json.schema.model.AbstractElement
-import kotlinx.serialization.json.JsonElement
 import kotlin.jvm.JvmStatic
 
 /**
- * The [FormatValidator] is used to check whether the [JsonElement] matches the expected format.
- * If the [JsonElement] is not of the required type (e.g. validator expects string but the [JsonElement] is an object)
+ * The [FormatValidator] is used to check whether the [AbstractElement] matches the expected format.
+ * If the [AbstractElement] is not of the required type (e.g. validator expects string but the [AbstractElement] is an object)
  * the validator **MUST** return [FormatValidator.Valid] result
  */
 @ExperimentalApi
