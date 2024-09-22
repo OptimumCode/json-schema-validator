a2b16bdf36547f56317e789b9588297063b4d5cc
Oleg Smirnov <oleg31101996@gmail.com>
2024-09-22 15:31:13 +0500

    Update JSON schema test-suite (#200)
    Automated update
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- test-suites/src/commonTest/kotlin/io/github/optimumcode/json/schema/suite/draft2020_12/TestSuite.kt
+++ test-suites/src/commonTest/kotlin/io/github/optimumcode/json/schema/suite/draft2020_12/TestSuite.kt
@@ -1,6 +1,6 @@
 package io.github.optimumcode.json.schema.suite.draft2020_12
 
-import io.github.optimumcode.json.schema.suite.COMMON_FORMAT_FILTER
+import io.github.optimumcode.json.schema.suite.TestFilter
 import io.github.optimumcode.json.schema.suite.runTestSuites
 import io.kotest.core.spec.style.FunSpec
 
@@ -9,7 +9,14 @@ internal class TestSuite : FunSpec() {
   init {
     runTestSuites(
       draftName = "draft2020-12",
-      formatFilter = COMMON_FORMAT_FILTER,
+      formatFilter =
+        TestFilter(
+          excludeSuites =
+            mapOf(
+              // Kotlin regex is not ECMA script
+              "ecmascript-regex" to setOf(),
+            ),
+        ),
     )
   }
 }
