92d621515a6e46b9ae83f7601f99d7523fd87261
Oleg Smirnov <oleg31101996@gmail.com>
2024-10-14 13:27:27 +0400

    Try to migrate to HttpClient for Java requests

--- json-schema-validator/src/jvmTest/kotlin/io/github/optimumcode/json/schema/JsonSchemaStreamTest.kt
+++ json-schema-validator/src/jvmTest/kotlin/io/github/optimumcode/json/schema/JsonSchemaStreamTest.kt
@@ -3,7 +3,11 @@ package io.github.optimumcode.json.schema
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.FunSpec
 import kotlinx.serialization.ExperimentalSerializationApi
-import java.net.URL
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+import java.time.Duration
 
 @OptIn(ExperimentalSerializationApi::class)
 @Suppress("unused")
@@ -11,9 +15,16 @@ class JsonSchemaStreamTest : FunSpec() {
   init {
     test("definition is loaded from input stream") {
       shouldNotThrowAny {
-        URL("https://json-schema.org/draft-07/schema#").openStream().use { input ->
-          JsonSchema.fromStream(input)
-        }
+        val client = HttpClient.newHttpClient()
+        val response =
+          client.send(
+            HttpRequest.newBuilder(URI.create("https://json-schema.org/draft-07/schema#"))
+              .GET()
+              .timeout(Duration.ofSeconds(10))
+              .build(),
+            BodyHandlers.ofInputStream(),
+          )
+        response.body().use(JsonSchema::fromStream)
       }
     }
   }
