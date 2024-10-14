04882bb992b5be46fe5c06444e94fa32c0210fff
Oleg Smirnov <oleg31101996@gmail.com>
2024-10-14 13:43:00 +0400

    Add redirect following for http client

--- json-schema-validator/src/jvmTest/kotlin/io/github/optimumcode/json/schema/JsonSchemaStreamTest.kt
+++ json-schema-validator/src/jvmTest/kotlin/io/github/optimumcode/json/schema/JsonSchemaStreamTest.kt
@@ -1,5 +1,6 @@
 package io.github.optimumcode.json.schema
 
+import io.kotest.assertions.asClue
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.FunSpec
 import kotlinx.serialization.ExperimentalSerializationApi
@@ -14,17 +15,24 @@ import java.time.Duration
 class JsonSchemaStreamTest : FunSpec() {
   init {
     test("definition is loaded from input stream") {
-      shouldNotThrowAny {
-        val client = HttpClient.newHttpClient()
-        val response =
-          client.send(
-            HttpRequest.newBuilder(URI.create("https://json-schema.org/draft-07/schema#"))
-              .GET()
-              .timeout(Duration.ofSeconds(10))
-              .build(),
-            BodyHandlers.ofInputStream(),
-          )
-        response.body().use(JsonSchema::fromStream)
+
+      val client =
+        HttpClient.newBuilder()
+          .followRedirects(HttpClient.Redirect.NORMAL)
+          .build()
+
+      val response =
+        client.send(
+          HttpRequest.newBuilder(URI.create("https://json-schema.org/draft-07/schema#"))
+            .GET()
+            .timeout(Duration.ofSeconds(10))
+            .build(),
+          BodyHandlers.ofInputStream(),
+        )
+      "Response code: ${response.statusCode()}".asClue {
+        shouldNotThrowAny {
+          response.body().use(JsonSchema::fromStream)
+        }
       }
     }
   }
