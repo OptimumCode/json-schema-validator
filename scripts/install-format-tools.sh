2c362e65fa1ba372a16f7b9be919bfaa85443f82
Oleg Smirnov <oleg31101996@gmail.com>
2025-01-07 19:24:45 +0400

    Use custom path for tools

--- scripts/install-format-tools.sh
+++ scripts/install-format-tools.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 if [[ "$DETEKT_VERSION" == "" ]]; then
 	echo "Privide detekt version in DETEKT_VERSION env variable"
 	exit 1
@@ -7,13 +10,14 @@ if [[ "$KTLINT_VERSION" == "" ]]; then
 	echo "Provide ktlint version in KTLINT_VERSION env variable"
 	exit 1
 fi
-mkdir -p ${HOME}/.local/bin
+TOOLS="$(pwd)/tools"
+mkdir -p ${TOOLS}
 echo "Installing detekt $DETEKT_VERSION"
-jar_destination="${HOME}/.local/lib/detekt"
+jar_destination="${TOOLS}/lib/detekt"
 mkdir -p ${jar_destination}
 curl -sSL -o ${jar_destination}/detekt-cli.jar "https://github.com/detekt/detekt/releases/download/v${DETEKT_VERSION}/detekt-cli-${DETEKT_VERSION}-all.jar"
 
-entrypoint_script="${HOME}/.local/bin/detekt-cli"
+entrypoint_script="${TOOLS}/detekt-cli"
 cat > "$entrypoint_script" << EOM
 java -jar ${jar_destination}/detekt-cli.jar \$@
 EOM
@@ -21,5 +25,7 @@ chmod +x "$entrypoint_script"
 detekt-cli --version
 
 echo "Installing ktlint $KTLINT_VERSION"
-curl -sSLO "https://github.com/pinterest/ktlint/releases/download/$KTLINT_VERSION/ktlint" && chmod a+x ktlint && mv ktlint ${HOME}/.local/bin/
+curl -sSLO "https://github.com/pinterest/ktlint/releases/download/$KTLINT_VERSION/ktlint" && chmod a+x ktlint && mv ktlint ${TOOLS}/
 ktlint --version
+
+export PATH="$PATH:$TOOLS"
