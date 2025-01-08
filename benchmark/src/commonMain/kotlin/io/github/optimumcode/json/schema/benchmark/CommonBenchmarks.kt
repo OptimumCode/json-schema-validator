ce4609dc5533f92387c81f969a66bfb9eec16dea
Oleg Smirnov <oleg31101996@gmail.com>
2025-01-08 18:48:18 +0400

    Correct usage of typealias in common code for benchmarks

--- benchmark/src/commonMain/kotlin/io/github/optimumcode/json/schema/benchmark/CommonBenchmarks.kt
+++ benchmark/src/commonMain/kotlin/io/github/optimumcode/json/schema/benchmark/CommonBenchmarks.kt
@@ -2,8 +2,7 @@ package io.github.optimumcode.json.schema.benchmark
 
 import kotlinx.benchmark.BenchmarkMode
 import kotlinx.benchmark.BenchmarkTimeUnit
-import kotlinx.benchmark.Mode.AverageTime
-import kotlinx.benchmark.Mode.Throughput
+import kotlinx.benchmark.Mode
 import kotlinx.benchmark.OutputTimeUnit
 import kotlinx.benchmark.Param
 import kotlinx.benchmark.Scope
@@ -11,7 +10,7 @@ import kotlinx.benchmark.State
 
 @State(Scope.Benchmark)
 @OutputTimeUnit(BenchmarkTimeUnit.SECONDS)
-@BenchmarkMode(Throughput)
+@BenchmarkMode(Mode.Throughput)
 class CommonThroughputBench : AbstractCommonBenchmark() {
   @Param("object")
   override var objectPath: String = ""
@@ -22,7 +21,7 @@ class CommonThroughputBench : AbstractCommonBenchmark() {
 
 @State(Scope.Benchmark)
 @OutputTimeUnit(BenchmarkTimeUnit.MICROSECONDS)
-@BenchmarkMode(AverageTime)
+@BenchmarkMode(Mode.AverageTime)
 class CommonAvgTimeBench : AbstractCommonBenchmark() {
   @Param("object")
   override var objectPath: String = ""
