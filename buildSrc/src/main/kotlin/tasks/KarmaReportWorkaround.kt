998c0f2fd9fc874cfa1bfb45a991ff2d0a48fba1
Oleg Smirnov <oleg31101996@gmail.com>
2026-02-15 15:39:44 +0100

    remove unused import

--- buildSrc/src/main/kotlin/tasks/KarmaReportWorkaround.kt
+++ buildSrc/src/main/kotlin/tasks/KarmaReportWorkaround.kt
@@ -3,7 +3,6 @@ package tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 /**
